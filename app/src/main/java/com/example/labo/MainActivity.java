b31873aa4d25531f45f246eff9b6e1a02cd5ffa7
marieThielens <thielens.marie@gmail.com>
2021-06-22 08:43:38 -0700

    upgrade

--- app/src/main/java/com/example/labo/MainActivity.java
+++ app/src/main/java/com/example/labo/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.labo.adapters.ActivityAdapter;
@@ -47,10 +48,15 @@ public class MainActivity extends AppCompatActivity {
         monRecycler = findViewById(R.id.rv_activity);
         btnAdd = findViewById(R.id.btn_add);
 
-        btnAdd.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), AjouterActivity.class );
-            startActivity(intent);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ouvrirActivity();
+            }
         });
+        // Adapter
+        activityAdapter = new ActivityAdapter(this, activities);
+
 
         // RecyclerView ...................
         // Liaison avec le Layout
@@ -63,4 +69,25 @@ public class MainActivity extends AppCompatActivity {
         monRecycler.setHasFixedSize(false); // Si des éléments sont rajouté ou enlevé, la modification du contenu de l'adapteur ne modifie ni sa hauteur ni sa largeur
         monRecycler.setAdapter(activityAdapter);
     }
+    private void ouvrirActivity() {
+        Intent intent = new Intent(getApplicationContext(), AjouterActivity.class);
+        startActivity(intent);
+    }
+    private void dbActivite(){
+        // Récupération des tâches
+        ActivityStructureDb activityDAO = new ActivityStructureDb(getApplicationContext());
+
+        Activity a1 = new Activity(1, "course","26/09/98" );
+
+        // lecture de la db
+        activityDAO.ouvrirLecture();
+        List<Activity> data = activityDAO.getAll();
+        activityDAO.close();
+
+        // Mise à jour de l'adapter et de la RecyclerView
+        activities.clear();
+        activities.addAll(data);
+        activityAdapter.notifyDataSetChanged(); // Prévenir mon adapter
+    }
+
 }
